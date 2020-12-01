43e56c08abb35fad669292b514a87bd9cae9ab99
Anja Thiart <anjathiart@gmail.com>
2020-12-01 08:49:02 +0200

    Factored error handling on joi definitions into joiError function

--- validate/definitions.js
+++ validate/definitions.js
@@ -1,104 +1,73 @@
 /* eslint-disable */
 const Joi = require('joi');
 
+const joiError = (msg) => {
+	error = new Error;
+	error.msg = msg;
+	throw error;
+}
 
 module.exports = Joi.object({
 	userIndex: Joi.number().integer().min(0).error(errors => {
-		if (errors.length > 0) {
-			error = new Error;
-			error.msg = 'userIndex must be a positive integer';
-			throw error;
-		}
+		joiError('userIndex must be a positive integer')
 	}),
 	userFullname: Joi.string().trim().min(3).max(64).error(errors => {
-		if (errors.length > 0) {
-			error = new Error;
-			error.msg = 'userFullname must be a string of at least 3 characters';
-			throw error;
-		}
+		joiError('userFullname must be a string of at least 3 characters')
 	}),
 	userEmail: Joi.string().trim().lowercase().email({ minDomainSegments: 2 }).error(errors => {
-		if (errors.length > 0) {
-			error = new Error;
-			error.msg = 'userEmail must be a valid email';
-			throw error;
-		}
+		joiError('userEmail must be a valid email')
 	}),
 	userPass: Joi.string().trim().min(8).max(128).error(errors => {
-		if (errors.length > 0) {
-			error = new Error;
-			error.msg = 'userPass must be a at least 8 characters';
-			throw error;
-		}
+		joiError('userPass must be a at least 8 characters')
 	}),
 	sessionIndex: Joi.number().integer().min(0).optional().error(errors => {
-		if (errors.length > 0) {
-			error = new Error;
-			error.msg = 'sessionIndex must be a positive integer';
-			throw error;
-		}
+		joiError('sessionIndex must be a positive integer')
 	}),
 	page: Joi.number().integer().min(1).optional().error(errors => {
-		error = new Error;
-		error.msg = 'page must be at least one character';
-		throw error;
+		joiError('page must be an integer of at least 1')
 	}),
 	limit: Joi.number().integer().min(1).max(1000).optional().error(errors => {
-		error = new Error;
-		error.msg = 'limit must be an integer between 1 and 1000';
-		throw error;
+		joiError('limit must be an integer between 1 and 1000')
 	}),
 	search: Joi.string().trim().allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'search string is invalid';
-		throw error;
+		joiError('search string is invalid')
 	}),
 	order: Joi.string().trim().allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'order string is invalid';
-		throw error;
+		joiError('order string is invalid')
 	}),
 	category: Joi.number().integer().min(0).allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'category must be a positive integer';
-		throw error;
+		joiError('category must be a positive integer')
 	}),
 	priceFrom: Joi.number().min(0).allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'priceFrom must be a positive number';
-		throw error;
+		joiError('priceFrom must be a positive number')
 	}),
 	priceTo: Joi.number().min(1).allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'priceTo be a number greater than 1';
-		throw error;
+		joiError('priceTo be a number greater than 1')
 	}),
 	inStock: Joi.boolean().allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'inStock must either a valid boolean (true / false)';
-		throw error;
+		joiError('inStock must either a valid boolean (true / false)')
 	}),
 	quantity: Joi.number().integer().min(1).allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'quantity must be a integer at least equal to 1';
-		throw error;
+		joiError('quantity must be a integer at least equal to 1')
 	}),
 	categoryIndex: Joi.number().integer().min(1).allow('').optional().error(errors => {
-		error = new Error;
-		error.msg = 'category must be a integer at least equal to 1';
-		throw error;
+		joiError('category must be a integer at least equal to 1')
 	}),
 	productIndex: Joi.number().integer().min(1).error(errors => {
-		error = new Error;
-		error.msg = 'productIndex must be a integer at least equal to 1';
-		throw error;
+		joiError('productIndex must be a integer at least equal to 1')
 	}),
 	itemIndex: Joi.number().integer().min(1).error(errors => {
-		error = new Error;
-		error.msg = 'itemIndex must be a integer at least equal to 1';
-		throw error;
-	})
-
-
+		joiError('itemIndex must be a integer at least equal to 1')
+	}),
+	editItems: Joi.array().items(Joi.object({
+		itemIndex: Joi.number().integer().min(1).required().error(errors => {
+			joiError('itemIndex must be a integer at least equal to 1')
+		}),
+		quantity: Joi.number().integer().min(1).allow('').required().error(errors => {
+			joiError('quantity must be a integer at least equal to 1')
+		}),
+	})).error(errors => {
+		joiError('editItems is invalid')
+	}),
 });
 
